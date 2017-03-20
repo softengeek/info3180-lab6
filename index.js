c024a94f0915b09350c289c64f12b18c1bed7fac
softengeek <softengeekkhadz@gmail.com>
2017-03-20 01:55:56 +0000

    step 3

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ app.controller("myCtrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
     $scope.addItem = function () {
         $scope.products.push($scope.addMe);
+    }
+    $scope.removeItem = function (item) {
+        $scope.products.splice(item, 1);
     } 
 });
