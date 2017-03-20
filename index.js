52736a6afe7c29230dd462b8bd79871c81a382c0
softengeek <softengeekkhadz@gmail.com>
2017-03-20 01:57:01 +0000

    step 4

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var app = angular.module("myShoppingList", []);
 app.controller("myCtrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
     $scope.addItem = function () {
-        $scope.products.push($scope.addMe);
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
     }
     $scope.removeItem = function (item) {
+        $scope.errortext = "";
         $scope.products.splice(item, 1);
     } 
 });
